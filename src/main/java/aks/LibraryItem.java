c8a9887b53b6d6f5c9a19c418fa2138c23a849bb
Alexander Karppi Säterö <alexander.karppisatero@elev.ga.ntig.se>
2026-05-06 10:13:30 +0200

    Initial commit

---
+++ src/main/java/aks/LibraryItem.java
@@ -0,0 +1,17 @@
+package aks;
+/*  Alexander Karppi Säterö
+    Klassen LibraryItem
+ */
+public class LibraryItem {
+    private String id;
+    private String title;
+    private boolean isAvailable;
+
+    // Getters
+    public String getId() {return id;}
+
+    public String getTitle() {return title;}
+
+    public boolean isAvailable() {return isAvailable;}
+
+}
---
+++ src/main/java/aks/Main.java
@@ -0,0 +1,9 @@
+package aks;
+/*  Alexander Karppi Säterö 
+    Programmet kan ...
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
